1695a3a216fbd6fb5aca68c0285b869002fa2389
Ana <anaiv9cs@yahoo.co.uk>
2021-10-30 15:04:08 +0300

    loadConfigurationFile method implemented

--- src/main/java/com/code/httpserver/config/ConfigurationManager.java
+++ src/main/java/com/code/httpserver/config/ConfigurationManager.java
@@ -1,5 +1,12 @@
 package com.code.httpserver.config;
 
+import com.code.httpserver.util.Json;
+import com.fasterxml.jackson.databind.JsonNode;
+
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
 /* singleton class*/
 public class ConfigurationManager {
     private static ConfigurationManager myConfigurationManager;
@@ -16,8 +23,15 @@ public class ConfigurationManager {
     }
 
     /* used for loading a config file by the path provider*/
-    public void loadConfigurationFile(String filePath){
-
+    public void loadConfigurationFile(String filePath) throws IOException {
+        FileReader fileReader = new FileReader(filePath);
+        StringBuffer sb = new StringBuffer();
+        int i;
+        while((i = fileReader.read()) != -1){
+            sb.append((char)i);
+        }
+        JsonNode conf = Json.parse(sb.toString());
+        myCurrentConfiguration = Json.fromJson(conf, Configuration.class);
     }
 
     /* returns the current loaded config*/

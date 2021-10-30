acbf9ec7fcaaa4e0e5d606088f4ec1571e7c17e1
Ana <anaiv9cs@yahoo.co.uk>
2021-10-30 16:09:32 +0300

    loading page from a different thread

--- src/main/java/com/code/httpserver/HttpServer.java
+++ src/main/java/com/code/httpserver/HttpServer.java
@@ -2,6 +2,9 @@ package com.code.httpserver;
 
 import com.code.httpserver.config.Configuration;
 import com.code.httpserver.config.ConfigurationManager;
+import com.code.httpserver.core.ServerListenerThread;
+
+import java.io.IOException;
 
 /* Driver class for the Http Server*/
 public class HttpServer {
@@ -10,7 +13,15 @@ public class HttpServer {
         System.out.println("hei");
         ConfigurationManager.getInstance().loadConfigurationFile("src/main/resources/http.json");
         Configuration conf = ConfigurationManager.getInstance().getCurrentConfiguration();
-        System.out.println("Using Port" + conf.getPort());
+        System.out.println("Using Port: " + conf.getPort());
         System.out.println("Using Webroot: " + conf.getWebroot());
+
+        try{
+            ServerListenerThread serverListenerThread = new ServerListenerThread(conf.getPort(), conf.getWebroot());
+            serverListenerThread.start();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+
     }
 }
---
+++ src/main/java/com/code/httpserver/core/ServerListenerThread.java
@@ -0,0 +1,49 @@
+package com.code.httpserver.core;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class ServerListenerThread extends Thread{
+   private int port;
+   private String webroot;
+   private ServerSocket serverSocket;
+
+   public ServerListenerThread(int port, String webroot) throws IOException {
+       this.port = port;
+       this.webroot = webroot;
+       this.serverSocket = new ServerSocket(this.port);
+   }
+
+    /*listening to a certain port and accept any connections that arise*/
+   public void run(){
+       try {
+           Socket socket = serverSocket.accept();
+
+           InputStream inputStream = socket.getInputStream();
+           OutputStream outputStream = socket.getOutputStream();
+
+           String html = "<html><head><title>Simple Java HTTP Server </title></head><body><h1>This page was served with my server</h1></body></html>";
+           final String CRLF = "\r\n"; //13,10
+           String response =
+                   "HTTP/1.1 200 OK" + CRLF + //Status line: http version response_code response_message
+                           "Content-Length: " + html.getBytes().length + CRLF + //header
+                           CRLF +
+                           html +
+                           CRLF + CRLF;
+
+           outputStream.write(response.getBytes());
+
+           inputStream.close();
+           outputStream.close();
+           socket.close();
+           serverSocket.close();
+
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
+   }
+
+}
